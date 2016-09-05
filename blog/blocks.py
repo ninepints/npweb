81d982f0cf862220305d10ca3a3dc1fecee023b8
ninepints <dev@ninepints.co>
2016-09-04 19:14:48 -0700

    Added "about" page
    -Moved some code and templates in the process
    -BasePages can no longer be created directly
    -Updated migrations file

--- blog/blocks.py
+++ blog/blocks.py
@@ -1,8 +1,14 @@
+import re
+
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 
-from wagtail.wagtailcore.blocks import ChoiceBlock, StructBlock, TextBlock
+from wagtail.wagtailcore.blocks import ChoiceBlock, RichTextBlock, StructBlock, TextBlock
+from wagtail.wagtailcore.blocks.stream_block import StreamBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+from wagtail.wagtailembeds.blocks import EmbedBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class CodeBlock(StructBlock):
@@ -23,3 +29,44 @@ class CodeBlock(StructBlock):
     class Meta:
         icon = 'code'
         template = 'blog/blocks/code.html'
+
+
+class ContentBlock(StreamBlock):
+    text = RichTextBlock()
+    image = ImageChooserBlock()
+    embed = EmbedBlock()
+    document = DocumentChooserBlock()
+    code = CodeBlock()
+
+
+class ContentMethodsMixin(object):
+    content_field_name = 'body'
+
+    @property
+    def content_field(self):
+        return getattr(self, self.content_field_name)
+
+    @property
+    def first_text_block(self):
+        try:
+            return next(block for block in self.content_field if isinstance(block.block, RichTextBlock))
+        except StopIteration:
+            return None
+
+    @property
+    def contains_math(self):
+        return any(self.block_contains_math(block) for block in self.content_field)
+
+    @property
+    def first_text_block_contains_math(self):
+        first_text_block = self.first_text_block
+        return first_text_block is not None and self.block_contains_math(first_text_block)
+
+    @staticmethod
+    def block_contains_math(block):
+        return (isinstance(block.block, RichTextBlock) and
+                re.search(r'(?:\\\[.*\\\])|(?:\\\(.*\\\))', block.value.source))
+
+    @property
+    def contains_code(self):
+        return any(isinstance(block.block, CodeBlock) for block in self.content_field)
