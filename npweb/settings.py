a2e2f88645bdb8c5b6bda11e8af99145b8e00564
ninepints <dev@ninepints.co>
2017-08-12 00:01:30 -0700

    Add Google analytics JS

--- npweb/settings.py
+++ npweb/settings.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = [
     'testserver',
 ]
 
+ENABLE_ANALYTICS = True
+
 
 # Application definition
 
@@ -76,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'npweb.context_processors.analytics',
             ],
         },
     },
